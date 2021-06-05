47c821c0bb3941ba4d1ec199f3022bc12ae17917
summerstime <shanasummers1@gmail.com>
2021-06-05 16:16:22 -0500

    Mapping GeoJSON Points

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,57 @@
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// An array which will be used to store created cityMarkers
+var cityMarkers = [];
+
+for (var i = 0; i < cities.length; i++) {
+  // loop through the cities array, create a new marker, push it to the cityMarkers array
+  cityMarkers.push(
+    L.marker(cities[i].location).bindPopup("<h1>" + cities[i].name + "</h1>")
+  );
+}
+
+// Add all the cityMarkers to a new layer group.
+// Now we can handle them as one group instead of referencing each individually
+var cityLayer = L.layerGroup(cityMarkers);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/summerstime/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function (data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    onEachFeature: function(features, layer){
+      layer.bindPopup("<h3>Airport Code: " + features.properties.faa + "</h3> <hr> <h4>Airport Name: " + features.properties.name + "</h4>")
+    }
+  }).addTo(map);
+});
+
